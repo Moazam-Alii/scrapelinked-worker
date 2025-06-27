7f3d527c6c018afe65039ff458f17484ab46ba5d
Moazam Ali <amoazam589@gmail.com>
2025-06-27 21:05:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def process_linkedin_posts():
 def clean_post_text(client, text):
     prompt = f"""
 You are a smart content cleaner. Given the following LinkedIn post content, extract only the useful text that seems like the main body of the post and try not to add the comments of the post also dont add the bio of profiles. Ignore these keywords and metadata: followers, reactions, comments, reply, student at, like, 1h, 2h, 3h, minutes ago, contact us.
-
+But dont change the body of the post content use exact same words same emojis(if any).
 --- Raw Text ---
 {text}
 
